c87d600af8777a26a946fe969707f61c6f249c38
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-12-14 23:05:35 +0300

    опт

--- bank_FF.asm
+++ bank_FF.asm
@@ -621,6 +621,10 @@ C - - - - - 0x01C297 07:C287: 4C 44 C3  JMP loc_C344_свап_prg_банка_80x
 sub_0x01C296:
                                         LDA #$1C
                                         JMP loc_C344_свап_prg_банка_80xx
+.export sub_0x01C297
+sub_0x01C297:
+                                        LDA #$1D
+                                        JMP loc_C344_свап_prg_банка_80xx
 sub_C24E:
 C - - - - - 0x01C25E 07:C24E: A9 1E     LDA #$1E
 C - - - - - 0x01C260 07:C250: 4C 44 C3  JMP loc_C344_свап_prg_банка_80xx
