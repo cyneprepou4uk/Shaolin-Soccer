97bcd157e223b6057a06e149f10d6ba0878fc4a9
cyneprepou4uk <cyneprepou4uk@gmail.com>
2021-03-31 22:00:02 +0300

    ком

--- bank_01.asm
+++ bank_01.asm
@@ -14361,8 +14361,7 @@ C D 1 - - - 0x007A85 01:BA75: 8D 03 04  STA ram_погода_spd_X_hi
 C - - - - - 0x007A88 01:BA78: 20 05 C0  JSR sub_0x01EE45_вращение_рандома
 C - - - - - 0x007A8B 01:BA7B: 10 05     BPL bra_BA82
 C - - - - - 0x007A8D 01:BA7D: A9 02     LDA #$02
-; bzk опт
-C - - - - - 0x007A8F 01:BA7F: 4C 84 BA  JMP loc_BA84
+C - - - - - 0x007A8F 01:BA7F: 4C 84 BA  JMP loc_BA84    ; bzk опт
 bra_BA82:
 - - - - - - 0x007A92 01:BA82: A9 FE     LDA #$FE
 loc_BA84:
