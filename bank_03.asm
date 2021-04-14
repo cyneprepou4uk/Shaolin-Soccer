65d1e5924fbd882570a8429a836140c241fb7239
PacnyTHuk <80532220+PacnyTHuk@users.noreply.github.com>
2021-04-15 00:56:07 +0600

    опт

--- bank_03.asm
+++ bank_03.asm
@@ -8532,15 +8532,15 @@ C - - - - - 0x00F18D 03:B17D: 90 03     BCC bra_B182
 C - - - - - 0x00F18F 03:B17F: 4C 87 B1  JMP loc_B187
 bra_B182:
 C - - - - - 0x00F192 03:B182: A9 00     LDA #$00
-C - - - - - 0x00F194 03:B184: 4C 92 B1  JMP loc_B192
+C - - - - - 0x00F194 03:B184: 4C 92 B1  BEQ bra_B192
 loc_B187:
 C D 1 - - - 0x00F197 03:B187: A5 1C     LDA ram_001C
 C - - - - - 0x00F199 03:B189: 10 05     BPL bra_B190
 C - - - - - 0x00F19B 03:B18B: A9 02     LDA #$02
-C - - - - - 0x00F19D 03:B18D: 4C 92 B1  JMP loc_B192
+C - - - - - 0x00F19D 03:B18D: 4C 92 B1  BNE bra_B192
 bra_B190:
 C - - - - - 0x00F1A0 03:B190: A9 01     LDA #$01
-loc_B192:
+bra_B192:
 C D 1 - - - 0x00F1A2 03:B192: 85 20     STA ram_0020
 C - - - - - 0x00F1A4 03:B194: A5 2B     LDA ram_002B
 C - - - - - 0x00F1A6 03:B196: F0 07     BEQ bra_B19F
@@ -8549,7 +8549,7 @@ C - - - - - 0x00F1AA 03:B19A: 90 03     BCC bra_B19F
 C - - - - - 0x00F1AC 03:B19C: 4C A4 B1  JMP loc_B1A4
 bra_B19F:
 C - - - - - 0x00F1AF 03:B19F: A9 00     LDA #$00
-C - - - - - 0x00F1B1 03:B1A1: 4C CB B1  JMP loc_B1CB
+C - - - - - 0x00F1B1 03:B1A1: 4C CB B1  BEQ bra_B1CB
 loc_B1A4:
 C D 1 - - - 0x00F1B4 03:B1A4: A5 1D     LDA ram_001D
 C - - - - - 0x00F1B6 03:B1A6: 10 13     BPL bra_B1BB
@@ -8562,7 +8562,7 @@ C - - - - - 0x00F1C2 03:B1B2: A9 00     LDA #$00
 C - - - - - 0x00F1C4 03:B1B4: 85 20     STA ram_0020
 bra_B1B6:
 C - - - - - 0x00F1C6 03:B1B6: A9 08     LDA #$08
-C - - - - - 0x00F1C8 03:B1B8: 4C CB B1  JMP loc_B1CB
+C - - - - - 0x00F1C8 03:B1B8: 4C CB B1  BNE bra_B1CB
 bra_B1BB:
 C - - - - - 0x00F1CB 03:B1BB: A5 20     LDA ram_0020
 C - - - - - 0x00F1CD 03:B1BD: F0 0A     BEQ bra_B1C9
@@ -8573,7 +8573,7 @@ C - - - - - 0x00F1D5 03:B1C5: A9 00     LDA #$00
 C - - - - - 0x00F1D7 03:B1C7: 85 20     STA ram_0020
 bra_B1C9:
 C - - - - - 0x00F1D9 03:B1C9: A9 04     LDA #$04
-loc_B1CB:
+bra_B1CB:
 C D 1 - - - 0x00F1DB 03:B1CB: 05 20     ORA ram_0020
 C - - - - - 0x00F1DD 03:B1CD: 85 20     STA ram_0020
 C - - - - - 0x00F1DF 03:B1CF: A8        TAY
